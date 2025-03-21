fdc6be4564bbacc79fb5d6de3890ea6a306ce7d2
Silakiniaina <sandasilakiniaina4@gmail.com>
2025-03-21 09:54:08 +0300

    feat : Create endpoint GET /data/reset

--- src/main/java/site/easy/to/build/crm/controller/DataFactoryController.java
+++ src/main/java/site/easy/to/build/crm/controller/DataFactoryController.java
@@ -1,8 +1,14 @@
 package site.easy.to.build.crm.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class DataFactoryController {
     
+    @GetMapping("/data/reset")
+    public String resetData(Model model) {
+        return "data/reset";
+    }
 }
