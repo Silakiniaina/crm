d263a40779ff20fecb1b187ad104478f6c0b1d75
Silakiniaina <sandasilakiniaina4@gmail.com>
2025-03-21 14:58:58 +0300

    feat : Create endpoint : GET /data/import

--- src/main/java/site/easy/to/build/crm/controller/DataImportController.java
+++ src/main/java/site/easy/to/build/crm/controller/DataImportController.java
@@ -1,8 +1,14 @@
 package site.easy.to.build.crm.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class DataImportController {
     
+    @GetMapping("/data/import")
+    public String importData(Model model) {
+        return "data/import";
+    }
 }
