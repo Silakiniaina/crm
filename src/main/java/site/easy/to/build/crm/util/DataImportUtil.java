ba037eb5febf9256a2eb9fa3ab2f8ea7a8a39ca4
Silakiniaina <sandasilakiniaina4@gmail.com>
2025-03-21 14:36:33 +0300

    feat : Create function checkHeaderMatchWithEntityField in DataImportUtil.

--- src/main/java/site/easy/to/build/crm/util/DataImportUtil.java
+++ src/main/java/site/easy/to/build/crm/util/DataImportUtil.java
@@ -1,5 +1,32 @@
 package site.easy.to.build.crm.util;
 
+import java.io.File;
+import java.io.FileReader;
+import java.lang.reflect.Field;
+import java.util.Arrays;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVFormat;
+
+import jakarta.persistence.Entity;
+
 public class DataImportUtil {
     
+    public static void checkHeaderMatchWithEntityField(File file, Class<?> entityClass) throws Exception {
+        if (!entityClass.isAnnotationPresent(Entity.class)) {
+            throw new IllegalArgumentException("Provided class is not an entity");
+        }
+        Set<String> entityFields = Arrays.stream(entityClass.getDeclaredFields())
+                .map(Field::getName)
+                .collect(Collectors.toSet());
+        try (CSVParser parser = new CSVParser(new FileReader(file), CSVFormat.DEFAULT.withFirstRecordAsHeader())) {
+            Set<String> csvHeaders = parser.getHeaderMap().keySet();
+
+            if (!csvHeaders.equals(entityFields)) {
+                throw new IllegalArgumentException("CSV headers do not match entity fields! Expected: " + entityFields + " but found: " + csvHeaders);
+            }
+        }
+    }
 }
