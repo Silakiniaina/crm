fd54d3ef0fbe5ad4e1e51bb5b260150a15501844
Silakiniaina <sandasilakiniaina4@gmail.com>
2025-03-21 11:48:50 +0300

    feat : Modify resetData() function

--- src/main/java/site/easy/to/build/crm/service/data/DataFactoryService.java
+++ src/main/java/site/easy/to/build/crm/service/data/DataFactoryService.java
@@ -3,5 +3,4 @@ package site.easy.to.build.crm.service.data;
 public interface DataFactoryService {
     
     public int resetData();
-
 }
--- src/main/java/site/easy/to/build/crm/service/data/DataFactoryServiceImpl.java
+++ src/main/java/site/easy/to/build/crm/service/data/DataFactoryServiceImpl.java
@@ -1,21 +1,40 @@
 package site.easy.to.build.crm.service.data;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import site.easy.to.build.crm.repository.data.DataFactoryRepository;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 
 @Service
 public class DataFactoryServiceImpl implements DataFactoryService{
 
-    private final DataFactoryRepository dataFactoryRepository;
+@PersistenceContext
+    private EntityManager entityManager;
 
-    public DataFactoryServiceImpl(DataFactoryRepository dataFactoryRepository) {
-        this.dataFactoryRepository = dataFactoryRepository;
-    }
-
+    @Transactional
     @Override
     public int resetData() {
-        return dataFactoryRepository.resetData();
+        entityManager.createNativeQuery("SET FOREIGN_KEY_CHECKS = 0").executeUpdate(); 
+        
+        entityManager.createNativeQuery("TRUNCATE TABLE users").executeUpdate();
+        entityManager.createNativeQuery("TRUNCATE TABLE oauth_users").executeUpdate();
+        entityManager.createNativeQuery("TRUNCATE TABLE user_profile").executeUpdate();
+        entityManager.createNativeQuery("TRUNCATE TABLE user_roles").executeUpdate();
+        entityManager.createNativeQuery("TRUNCATE TABLE employee").executeUpdate();
+        entityManager.createNativeQuery("TRUNCATE TABLE email_template").executeUpdate();
+        entityManager.createNativeQuery("TRUNCATE TABLE customer_login_info").executeUpdate();
+        entityManager.createNativeQuery("TRUNCATE TABLE customer").executeUpdate();
+        entityManager.createNativeQuery("TRUNCATE TABLE trigger_lead").executeUpdate();
+        entityManager.createNativeQuery("TRUNCATE TABLE trigger_ticket").executeUpdate();
+        entityManager.createNativeQuery("TRUNCATE TABLE trigger_contract").executeUpdate();
+        entityManager.createNativeQuery("TRUNCATE TABLE contract_settings").executeUpdate();
+        entityManager.createNativeQuery("TRUNCATE TABLE lead_action").executeUpdate();
+        entityManager.createNativeQuery("TRUNCATE TABLE lead_settings").executeUpdate();
+        entityManager.createNativeQuery("TRUNCATE TABLE ticket_settings").executeUpdate();
+        entityManager.createNativeQuery("TRUNCATE TABLE file").executeUpdate();
+        entityManager.createNativeQuery("TRUNCATE TABLE google_drive_file").executeUpdate();
+
+        return entityManager.createNativeQuery("SET FOREIGN_KEY_CHECKS = 1").executeUpdate();
     }
-    
 }
