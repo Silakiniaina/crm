3185328b004d554a281998a1a8bf9bd026ecb38d
Silakiniaina <sandasilakiniaina4@gmail.com>
2025-03-22 15:01:50 +0300

    feat : Create controller CustomerBudgetController

---
+++ src/main/java/site/easy/to/build/crm/controller/CustomerBudgetController.java
@@ -0,0 +1,10 @@
+package site.easy.to.build.crm.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/customer-budget")
+public class CustomerBudgetController {
+    
+}
