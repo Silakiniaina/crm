358038bde0f31bcba81bda573cbc1582a15243b9
Silakiniaina <sandasilakiniaina4@gmail.com>
2025-03-22 14:58:49 +0300

    feat : Override addBudget function on customerBudgetServiceImpl.

--- src/main/java/site/easy/to/build/crm/service/budget/CustomerBudgetServiceImpl.java
+++ src/main/java/site/easy/to/build/crm/service/budget/CustomerBudgetServiceImpl.java
@@ -2,7 +2,18 @@ package site.easy.to.build.crm.service.budget;
 
 import org.springframework.stereotype.Service;
 
+import lombok.AllArgsConstructor;
+import site.easy.to.build.crm.entity.CustomerBudget;
+import site.easy.to.build.crm.repository.CustomerBudgetRepository;
+
 @Service
-public class CustomerBudgetServiceImpl implememts CustomerBudgetService {
+@AllArgsConstructor
+public class CustomerBudgetServiceImpl implements CustomerBudgetService {
     
+    private final CustomerBudgetRepository customerBudgetRepository;
+    
+    @Override
+    public CustomerBudget addBudget(CustomerBudget customerBudget) throws Exception {
+        return customerBudgetRepository.save(customerBudget);
+    }
 }
