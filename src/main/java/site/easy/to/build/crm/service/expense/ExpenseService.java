68b17f6ea442d1162e8a938f98a41c915c1d99c4
Silakiniaina <sandasilakiniaina4@gmail.com>
2025-03-23 14:58:25 +0300

    feat : User function filter on ExpenseService.

--- src/main/java/site/easy/to/build/crm/service/expense/ExpenseService.java
+++ src/main/java/site/easy/to/build/crm/service/expense/ExpenseService.java
@@ -1,8 +1,12 @@
 package site.easy.to.build.crm.service.expense;
 
+import java.util.List;
+
 import site.easy.to.build.crm.entity.Expense;
 
 public interface ExpenseService {
     
     public Expense addExpense(Expense expense) throws Exception;
+
+    public List<Expense> findExpensesByFilters(Integer type, Integer id);
 }
