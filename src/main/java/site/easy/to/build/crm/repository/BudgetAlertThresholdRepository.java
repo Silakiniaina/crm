6042dc9fe0aafdb5b1990cfdfea7973f3a806bc6
Silakiniaina <sandasilakiniaina4@gmail.com>
2025-03-23 22:23:41 +0300

    feat : Create repository BudgetAlertThresholdRepository

---
+++ src/main/java/site/easy/to/build/crm/repository/BudgetAlertThresholdRepository.java
@@ -0,0 +1,12 @@
+package site.easy.to.build.crm.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import site.easy.to.build.crm.entity.BudgetAlertThreshold;
+
+@Repository
+public interface BudgetAlertThresholdRepository extends JpaRepository<BudgetAlertThreshold, Integer> {
+
+    
+}
