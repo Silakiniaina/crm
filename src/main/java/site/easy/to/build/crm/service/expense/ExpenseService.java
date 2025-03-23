7cd4ca97cc7599cf7103f61ef359de49b335b779
Silakiniaina <sandasilakiniaina4@gmail.com>
2025-03-23 12:35:45 +0300

    feat : Override function addExpense.

--- src/main/java/site/easy/to/build/crm/service/expense/ExpenseService.java
+++ src/main/java/site/easy/to/build/crm/service/expense/ExpenseService.java
@@ -1,5 +1,8 @@
 package site.easy.to.build.crm.service.expense;
 
+import site.easy.to.build.crm.entity.Expense;
+
 public interface ExpenseService {
     
+    public Expense addExpense(Expense expense) throws Exception;
 }
--- src/main/java/site/easy/to/build/crm/service/expense/ExpenseServiceImpl.java
+++ src/main/java/site/easy/to/build/crm/service/expense/ExpenseServiceImpl.java
@@ -1,8 +1,33 @@
 package site.easy.to.build.crm.service.expense;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import site.easy.to.build.crm.entity.Expense;
+import site.easy.to.build.crm.repository.ExpenseRepository;
 
 @Service
 public class ExpenseServiceImpl implements ExpenseService {
-    
+
+    private final ExpenseRepository expenseRepository;
+
+    @Autowired
+    public ExpenseServiceImpl(ExpenseRepository expenseRepository) {
+        this.expenseRepository = expenseRepository;
+    }
+
+    @Override
+    public Expense addExpense(Expense expense) throws Exception {
+        if (expense == null) {
+            throw new Exception("Expense cannot be null");
+        }
+        try {
+            return expenseRepository.save(expense);
+        } catch (Exception e) {
+            throw new Exception("Failed to save expense: " + e.getMessage());
+        }
+    }
 }
