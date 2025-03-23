827258b9473bc39096da94db35c4b744d937dfa2
Silakiniaina <sandasilakiniaina4@gmail.com>
2025-03-23 14:58:07 +0300

    feat : Add function filter.

--- src/main/java/site/easy/to/build/crm/repository/ExpenseRepository.java
+++ src/main/java/site/easy/to/build/crm/repository/ExpenseRepository.java
@@ -1,6 +1,10 @@
 package site.easy.to.build.crm.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import site.easy.to.build.crm.entity.Expense;
@@ -8,4 +12,11 @@ import site.easy.to.build.crm.entity.Expense;
 @Repository
 public interface ExpenseRepository extends JpaRepository<Expense, Integer> {
 
+    List<Expense> findByExpenseType(Integer expenseType);
+
+    @Query("SELECT e FROM Expense e WHERE e.lead.leadId = :id OR e.ticket.ticketId = :id")
+    List<Expense> findByRelatedId(@Param("id") Integer id);
+
+    @Query("SELECT e FROM Expense e WHERE e.expenseType = :type AND (e.lead.leadId = :id OR e.ticket.ticketId = :id)")
+    List<Expense> findByExpenseTypeAndRelatedId(@Param("type") Integer type, @Param("id") Integer id);
 }
