8f25c2343d31e3f12b68a21482bfa5b210b711b3
Silakiniaina <sandasilakiniaina4@gmail.com>
2025-03-24 10:54:15 +0300

    feat : Create endpoint : GET /api/data-toal

---
+++ src/main/java/site/easy/to/build/crm/controller/DataTotalController.java
@@ -0,0 +1,36 @@
+package site.easy.to.build.crm.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import site.easy.to.build.crm.util.Response;
+import site.easy.to.build.crm.util.ResponseUtil;
+import site.easy.to.build.crm.dto.TotalDataDTO;
+import site.easy.to.build.crm.service.dashboard.TotalDataService;
+
+@RestController
+@RequestMapping("/api")
+public class DataTotalController {
+
+    private final TotalDataService totalDataService;
+
+    @Autowired
+    public DataTotalController(TotalDataService totalDataService) {
+        this.totalDataService = totalDataService;
+    }
+
+    @SuppressWarnings("unchecked")
+    @GetMapping("/data-total")
+    public <T> ResponseEntity<Response<T>> getTotalData() {
+        try {
+            TotalDataDTO totalData = totalDataService.getTotalData();
+            return ResponseUtil.sendResponse(HttpStatus.OK, true, "Total data retrieved successfully", (T) totalData);
+        } catch (Exception e) {
+            return ResponseUtil.sendResponse(HttpStatus.BAD_REQUEST, false, "Error while fetching total data", (T) e.getMessage());
+        }
+    }
+}
