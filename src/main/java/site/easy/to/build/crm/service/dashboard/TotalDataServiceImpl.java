49a82648f0b7a2c02d2899aade012adbd74ef4e8
Silakiniaina <sandasilakiniaina4@gmail.com>
2025-03-24 11:52:42 +0300

    feat : Override List<?> getTotalDataDetails(int type); functions.

--- src/main/java/site/easy/to/build/crm/service/dashboard/TotalDataServiceImpl.java
+++ src/main/java/site/easy/to/build/crm/service/dashboard/TotalDataServiceImpl.java
@@ -1,22 +1,43 @@
 package site.easy.to.build.crm.service.dashboard;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import site.easy.to.build.crm.dto.TotalDataDTO;
+import site.easy.to.build.crm.repository.CustomerBudgetRepository;
+import site.easy.to.build.crm.repository.ExpenseRepository;
 import site.easy.to.build.crm.repository.TotalDataRepository;
 
 @Service
 public class TotalDataServiceImpl implements TotalDataService {
 
     private final TotalDataRepository totalDataRepository;
+    private final CustomerBudgetRepository customerBudgetRepository;
+    private final ExpenseRepository expenseRepository;
 
     @Autowired
-    public TotalDataServiceImpl(TotalDataRepository totalDataRepository) {
+    public TotalDataServiceImpl(CustomerBudgetRepository cus, ExpenseRepository exp,TotalDataRepository totalDataRepository) {
         this.totalDataRepository = totalDataRepository;
+        this.expenseRepository =exp;
+        this.customerBudgetRepository = cus;
     }
 
     @Override
     public TotalDataDTO getTotalData() {
         return totalDataRepository.getTotalData();
+    }
+
+    public List<?> getTotalDataDetails(int type) {
+        switch (type) {
+            case 0: 
+                return customerBudgetRepository.findAll();
+            case 1: 
+                return expenseRepository.findByTicketTicketIdIsNotNull();
+            case 2: 
+                return expenseRepository.findByLeadLeadIdIsNotNull();
+            default:
+                throw new IllegalArgumentException("Invalid type parameter: " + type);
+        }
     }
 }
