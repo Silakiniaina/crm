cbf9df2bdf91f194d591b7284695d0d254e08b81
Silakiniaina <sandasilakiniaina4@gmail.com>
2025-03-25 22:06:54 +0300

    feat : Create abstract method checkForeignKey()

--- src/main/java/site/easy/to/build/crm/util/data/importer/DataImport.java
+++ src/main/java/site/easy/to/build/crm/util/data/importer/DataImport.java
@@ -78,5 +78,6 @@ public abstract class DataImport {
 
     public abstract List<String> getValidStatus();
     public abstract void checkIntegrity();
+    public abstract void checkForeignKey();
 
 }
