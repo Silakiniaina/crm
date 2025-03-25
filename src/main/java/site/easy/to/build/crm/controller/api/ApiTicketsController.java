a75418122c1c0092ea6ea87a8643215e25680e26
Silakiniaina <sandasilakiniaina4@gmail.com>
2025-03-25 16:56:12 +0300

    feat : Create endpoint : DELETE /api/tickets/{id}

--- src/main/java/site/easy/to/build/crm/controller/api/ApiTicketsController.java
+++ src/main/java/site/easy/to/build/crm/controller/api/ApiTicketsController.java
@@ -2,10 +2,11 @@ package site.easy.to.build.crm.controller.api;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -32,6 +33,18 @@ public class ApiTicketsController {
             return ResponseUtil.sendResponse(HttpStatus.OK, true, "Tickets fetched successfully", (T)tickets);
         } catch (Exception e) {
             return ResponseUtil.sendResponse(HttpStatus.INTERNAL_SERVER_ERROR, false, "Error while fetching tickets", (T)e.getMessage());
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    @DeleteMapping("/{id}")
+    public <T> ResponseEntity<Response<T>> deleteTicket(@PathVariable("id") int id){
+        try {
+            Ticket ticket = ticketService.findByTicketId(id);
+            ticketService.delete(ticket);
+            return ResponseUtil.sendResponse(HttpStatus.OK, true, "Tickets deleted successfully", null);
+        } catch (Exception e) {
+            return ResponseUtil.sendResponse(HttpStatus.INTERNAL_SERVER_ERROR, false, "Error while deleting ticket id : "+id, (T)e.getMessage());
         }
     }
 
