46e25e429470b6235d4ebbd08d65673e9e6ffd6a
Silakiniaina <sandasilakiniaina4@gmail.com>
2025-03-25 16:19:29 +0300

    feat : Create controller API for tiecket.

---
+++ src/main/java/site/easy/to/build/crm/controller/api/ApiTicketsController.java
@@ -0,0 +1,10 @@
+package site.easy.to.build.crm.controller.api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/tickets")
+public class ApiTicketsController {
+    
+}
