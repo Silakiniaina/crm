7d59808b8194cdcaf45941ecfa47f0f92d0acfd0
Silakiniaina <sandasilakiniaina4@gmail.com>
2025-03-25 22:13:59 +0300

    feat : Add attribute email and name.

--- src/main/java/site/easy/to/build/crm/util/data/importer/CustomerDataImport.java
+++ src/main/java/site/easy/to/build/crm/util/data/importer/CustomerDataImport.java
@@ -2,7 +2,15 @@ package site.easy.to.build.crm.util.data.importer;
 
 import java.util.List;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class CustomerDataImport extends DataImport{
+
+    private String email;
+    private String name;
 
     @Override
     public void checkForeignKey() {
