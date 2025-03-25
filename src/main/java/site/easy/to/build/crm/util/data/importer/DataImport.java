a39144dc1d48702420a63e8346ae51a08d58d32e
Silakiniaina <sandasilakiniaina4@gmail.com>
2025-03-25 21:53:00 +0300

    feat : Create class DataImport.

---
+++ src/main/java/site/easy/to/build/crm/util/data/importer/DataImport.java
@@ -0,0 +1,11 @@
+package site.easy.to.build.crm.util.data.importer;
+
+import java.util.List;
+
+public abstract class DataImport {
+    
+    private List<String> errors;
+    private boolean valid;
+    private String fileName;
+
+}
