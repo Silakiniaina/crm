5ed3b414c33d1eab0e106b9b698d8b01ede84f44
Silakiniaina <sandasilakiniaina4@gmail.com>
2025-03-26 03:17:47 +0300

    feat : Remove old Import class.

--- src/main/java/site/easy/to/build/crm/service/data/ImportDataService.java
+++
@@ -1,9 +0,0 @@
-package site.easy.to.build.crm.service.data;
-
-import java.io.File;
-
-public interface ImportDataService {
-    
-    public void importData(File f , Class<?> entity) throws Exception;
-}
-
