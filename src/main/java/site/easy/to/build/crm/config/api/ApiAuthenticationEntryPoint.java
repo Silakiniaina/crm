db47291e642624fa5c34440380f92229e9352183
Silakiniaina <sandasilakiniaina4@gmail.com>
2025-03-26 10:10:10 +0300

    feat : Updata error message on Authentification error.

--- src/main/java/site/easy/to/build/crm/config/api/ApiAuthenticationEntryPoint.java
+++ src/main/java/site/easy/to/build/crm/config/api/ApiAuthenticationEntryPoint.java
@@ -15,7 +15,7 @@ package site.easy.to.build.crm.config.api;
                           AuthenticationException authException) throws IOException {
          response.setContentType("application/json");
          response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-         response.getWriter().write("{\"message\": \"Unauthorized - Invalid credentials or token\", \"status\": 403, \"success\": false, \"data\": null}");
+         response.getWriter().write("{\"message\": \"Unauthorized - Invalid credentials or token\", \"status\": 403, \"success\": false, \"data\": "+authException.getMessage()+"}");
          response.getWriter().flush();
      }
  }
