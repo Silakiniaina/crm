62144b2eed7d75b15650c9f0f4eb6c48426b3068
Silakiniaina <sandasilakiniaina4@gmail.com>
2025-03-26 10:11:23 +0300

    feat : Add validation on CustomerDataImporlt.

--- src/main/java/site/easy/to/build/crm/entity/CustomerBudget.java
+++ src/main/java/site/easy/to/build/crm/entity/CustomerBudget.java
@@ -11,6 +11,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
+import jakarta.validation.constraints.Min;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
